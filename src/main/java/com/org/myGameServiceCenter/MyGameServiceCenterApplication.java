61e5a64c2fda7006ccd4ca9ec2c17ae5231e0376
nielong <nl@qq.com>
2018-12-22 17:58:43 +0800

    first commit

---
+++ src/main/java/com/org/myGameServiceCenter/MyGameServiceCenterApplication.java
@@ -0,0 +1,14 @@
+package com.org.myGameServiceCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGameServiceCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGameServiceCenterApplication.class, args);
+	}
+
+}
+
